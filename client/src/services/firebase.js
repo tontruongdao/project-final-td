f7d7fc9e787fb297466c59028ab5113c9fae5f47
Truong Dao <truong.dao.do@gmail.com>
2020-10-17 16:37:03 -0400

    Firebase enabled 1.2

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -3,9 +3,9 @@ import firebase from 'firebase'
 // Firebase initial configuration.
 
 const config = {
-    apiKey: "AIzaSyAgjvqfSII-ZsivCQDGL0UKeBTcQibQeh4",
-    authDomain: "chatty-8c1a7.firebaseapp.com",
-    databaseURL: "https://chatty-8c1a7.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: ""
   };
   firebase.initializeApp(config);
   export const auth = firebase.auth;
